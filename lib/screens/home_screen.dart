53f644770dfd2e5f01db6049488d8f538cfb3ee7
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-07-09 12:43:45 +0500

    dsfchb

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location_permisson/services/location_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController myController;
+  final LatLng _center = const LatLng(39.0168, 125.7474);
+  final LatLng _najotTalim = const LatLng(41.2856806, 69.2034646);
+
+  void _onMapCreated(GoogleMapController controller) {
+    myController = controller;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -23,16 +32,31 @@ class _HomeScreenState extends State<HomeScreen> {
     final myLocation = LocationService.currentLocation;
     print(myLocation);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: const Text(
-          "Location Permission",
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text(
+            "Location Permission",
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(myLocation.toString()),
-      ),
-    );
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition:
+                  CameraPosition(target: _najotTalim, zoom: 15.0),
+              mapType: MapType.hybrid,
+              markers: {
+                Marker(
+                    markerId: const MarkerId("Najot Ta'lim"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _najotTalim,
+                    infoWindow: const InfoWindow(
+                        title: "Najot talim",
+                        snippet: "Xush kelibsiz Qirg'inbarot yurtga"))
+              },
+            ),
+          ],
+        ));
   }
 }
